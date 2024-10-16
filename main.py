5f0f5d59bd0f8a96e168fd5606e722eeb465951f
nycolasdiaas <nycolaskate2003@hotmail.com>
2024-10-16 00:07:53 -0300

    color_team_assigner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
+from team_assigner import TeamAssigner
 import cv2
 
 def main():
@@ -14,6 +15,18 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
     
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.teams_colors[team]
 
     # Draw output
     ## Draw object Tracks
